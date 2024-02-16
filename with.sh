5639203db52502cf55ad6b6c70cc475cf18c250c
Thierry Delamare <t.delamare@laposte.net>
2024-02-16 13:23:34 +0100

    Corrects missing with.decl

--- with.sh
+++ with.sh
@@ -3,4 +3,4 @@ alias decl=with.decl
 with () { until [[ $# == 1 || $1 == -- ]]; do decl $1; shift; done; if [[ $1 == -- ]]; then shift; fi; decl $1; echo $@; }
 unalias decl
 
-with.list () { echo with; }
+with.list () { echo with with.decl; }
